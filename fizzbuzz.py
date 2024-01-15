bad6d2fbd790b95016b97151860d770086686cc6
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2024-01-15 14:15:12 +0900

    feat: Print 'fizzbuzz' if int is multiple of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 def get_fizzbuzz() -> str:
     for i in range(1, 16):
-        if i % 3 == 0:
+        if i % 15 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
             print('fizz')
         elif i % 5 == 0:
             print('buzz')
