c7f7a9bc9062a53a37c56a688949876d069758f3
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2024-01-15 14:09:06 +0900

    feat: Print 'fizz' if int is multiple of 3 with range 1 to 15
    else print integer itself

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,9 @@
-print('Sucess of creating fizzbuzz.py')
+def get_fizzbuzz() -> str:
+    for i in range(1, 16):
+        if i % 3 == 0:
+            print('fizz')
+        else:
+            print(f'{i}')
+
+if __name__ == '__main__':
+    get_fizzbuzz()
